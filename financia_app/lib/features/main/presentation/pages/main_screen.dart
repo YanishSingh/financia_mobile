3e04bcddb78dfa85ebe9e4cbe39e67e04a94d5a6
YanishSingh <singhyanish999@gmail.com>
2025-03-06 23:58:09 +0545

    main screen.dart

---
+++ financia_app/lib/features/main/presentation/pages/main_screen.dart
@@ -0,0 +1,60 @@
+import 'package:financia_app/features/budget/presentation/pages/budget_page.dart';
+import 'package:financia_app/features/dashboard/presentation/pages/dashboard_page.dart';
+import 'package:financia_app/features/profile/presentation/pages/profile_page.dart';
+import 'package:financia_app/features/transaction/presentation/pages/transaction_page.dart';
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  // List of pages for each bottom navigation item.
+  final List<Widget> _pages = const [
+    DashboardPage(),
+    TransactionPage(),
+    BudgetPage(),
+    ProfilePage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Transactions',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.pie_chart),
+            label: 'Budgets',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
