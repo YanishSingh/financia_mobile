3b07a2d09e4d0fbd5e9055d7a6e3272030a2e437
YanishSingh <singhyanish999@gmail.com>
2025-03-06 23:54:55 +0545

    domain layer files

---
+++ financia_app/lib/features/dashboard/domain/entities/transaction.dart
@@ -0,0 +1,18 @@
+// lib/features/dashboard/domain/entities/transaction.dart
+class TransactionEntity {
+  final String id;
+  final String type; // 'income' or 'expense'
+  final String category;
+  final double amount;
+  final DateTime date;
+  final String? description;
+
+  TransactionEntity({
+    required this.id,
+    required this.type,
+    required this.category,
+    required this.amount,
+    required this.date,
+    this.description,
+  });
+}
