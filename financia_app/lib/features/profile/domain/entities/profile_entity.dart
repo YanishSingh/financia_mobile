972e3f61f1cdc5aa64e7396b15fde76dc72859f1
YanishSingh <singhyanish999@gmail.com>
2025-03-06 23:59:09 +0545

    profile

---
+++ financia_app/lib/features/profile/domain/entities/profile_entity.dart
@@ -0,0 +1,13 @@
+class ProfileEntity {
+  final String id;
+  final String name;
+  final String email;
+  final String profileImage;
+
+  ProfileEntity({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.profileImage,
+  });
+}
